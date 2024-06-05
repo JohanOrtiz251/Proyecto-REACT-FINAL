3e91c9c4085fd47461330edd9843779af1204291
JohanOrtiz251 <Johanortiz251@gmail.com>
2024-06-05 14:02:47 -0500

    Actualizando FINAL

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -15,6 +15,16 @@ const Login = ({ darkMode, toggleDarkMode }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+
+        /* const response = await fetch('http://localhost:3001/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
+            const data = await response.json(); */
+
       const response = await fetch(`${URL}/login`, {
         method: 'POST',
         headers: {
